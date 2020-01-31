8264a97e70907825067e0c3109a9b964bac478ee
Saskia Azzouzi <saskia.azzouzi@gmx.de>
2020-01-31 10:46:31 +0100

    Testzwecke

---
+++ Test10GitHub/Kunde.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test10GitHub
+{
+    class Kunde : Person
+    {
+
+        public decimal Umsatz;
+    }
+}
